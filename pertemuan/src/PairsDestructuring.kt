e7a1dae308bcfd85fd52fcac0d02d498bae106d2
farizz <fairizwalker@gmail.com>
2025-09-18 19:41:00 +0700

    tes

---
+++ pertemuan/src/PairsDestructuring.kt
@@ -0,0 +1,10 @@
+
+fun main(){
+    val firstUser: Pair<String, Byte> = Pair("User", 20)
+
+
+    val (username, age) = firstUser
+
+    println(username)
+    print(age)
+}
---
+++ pertemuan/src/TripleDestructuring.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val firstUser : Triple<String, Byte, String> = Triple("User 1", 20, "Famale")
+
+
+    val (username, age, gender) = firstUser
+    println(username)
+    println(age)
+    print(gender)
+}
